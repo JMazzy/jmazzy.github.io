63157e9ee25a20a03e70a866dde22a481393e878
Joshua Masland-Sarani <j.masland@gmail.com>
2016-03-16 19:29:39 -0400

    updates

--- games/seafarers_map_maker/js/hex_model.js
+++
@@ -1,9 +0,0 @@
-function Hex(kind, number) {
-  this.kind = kind;
-  this.number = number;
-
-  this.setPosition = function(row,col) {
-    this.position = (new Offset(row,col)).toCubic();
-    this.neighbors = hexMath.findNeighbors( this.position );
-  }
-}
